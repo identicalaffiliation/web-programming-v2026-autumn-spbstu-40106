e78c00ec4bfbe2ba58a3d36a3eb6bdb469476022
identicalaffiliation <fre1fsi.biz.c@gmail.com>
2026-09-07 14:55:10 +0300

    lab3

---
+++ mazitov.vladislav/lab3/solution.ts
@@ -0,0 +1,17 @@
+// Экспортируйте отсюда функцию с именем из контракта вашего варианта.
+export function findLongestIncreasingSubsequence(numbers: number[]): number {
+  if (numbers.length == 0) {
+    return 0;
+  }
+
+  const dp = new Array(Array.length).fill(1);
+  for (let i = 0; i < Array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (numbers[j] < numbers[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+}
