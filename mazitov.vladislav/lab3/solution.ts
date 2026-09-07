ccc2e057d9d8f2e6be727a269979b2a62cc33c61
identicalaffiliation <fre1fsi.biz.c@gmail.com>
2026-09-07 14:58:15 +0300

    fix

--- mazitov.vladislav/lab3/solution.ts
+++ mazitov.vladislav/lab3/solution.ts
@@ -4,8 +4,8 @@ export function findLongestIncreasingSubsequence(numbers: number[]): number {
     return 0;
   }
 
-  const dp = new Array(Array.length).fill(1);
-  for (let i = 0; i < Array.length; i++) {
+  const dp = new Array(numbers.length).fill(1);
+  for (let i = 0; i < numbers.length; i++) {
     for (let j = 0; j < i; j++) {
       if (numbers[j] < numbers[i]) {
         dp[i] = Math.max(dp[i], dp[j] + 1);
